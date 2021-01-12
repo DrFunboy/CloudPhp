b2a8d77b52d7e626952a427624457007dcd2efab
DrFunboy <Drfunboy47@gmail.com>
2021-01-12 15:11:05 +0300

    del sort

--- pages/do_load.php
+++ pages/do_load.php
@@ -5,10 +5,9 @@ header('Content-Type: text/html; charset=utf-8');
 $data = $_POST;
 if (isset($data["do_but_enter"]))
 {
-	$file_type = $_FILES ['file_inp']['type'];
 	$file_name = $_FILES ['file_inp']['name'];
 	$file_tmp_name = $_FILES ['file_inp']['tmp_name'];
-	$new_temp = "../files/another/";
+	$new_temp = "../files/";
 	$errors = array();
 	if (($file_name) == "")
 	{
@@ -16,42 +15,9 @@ if (isset($data["do_but_enter"]))
 	}
 	if (empty($errors))
 	{
-		if(stristr($file_type, 'image') == true)
-		{
+
 			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/images/". $file_name);
-			$new_temp = "../files/images/";
-		}
-		else if (stristr($file_type, 'text') == true)
-		{
-			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/text/". $file_name);
-			$new_temp = "../files/text/";
-		}
-		else if (stristr($file_type, 'application') == true)
-		{
-			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/application/". $file_name);
-			$new_temp = "../files/application/";
-		}
-		else if (stristr($file_type, 'video') == true)
-		{
-			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/video/". $file_name);
-			$new_temp = "../files/video/";
-		}
-		else if (stristr($file_type, 'audio') == true)
-		{
-			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/audio/". $file_name);
-			$new_temp = "../files/audio/";
-		}
-		else
-		{
-			echo(" <div style='color: #0f0;'>да! </div>");
-			move_uploaded_file($file_tmp_name, "../files/another/". $file_name);
-			$new_temp = "../files/another/";
-		}
+			move_uploaded_file($file_tmp_name, "../files/". $file_name);
 	}
 	else
 	{
@@ -66,7 +32,7 @@ if (isset($data["do_but_enter"]))
 	$client = new Arhitector\Yandex\Client\OAuth($authtoken);
 	$disk = new Arhitector\Yandex\Disk($client);
 	$resource = $disk->getResource('disk:/Загрузки/'.$file_name);
-	$resource->upload($new_temp. $file_name);
+	$resource->upload("../files/". $file_name);
 	$resource->public_url;
 	echo("<pre>");
 	var_dump($resource);
